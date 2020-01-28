43ea5694ed5b990ee31f88113ad244717a95ccf1
Sophie Vanderveken <svv@sii.be>
2020-01-28 14:50:57 +0100

    global component structure

---
+++ src/MainContent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function MainContent() {
+    return (
+        <div class="main">
+            <h3>
+                Main Here
+            </h3>
+        </div>
+    );
+}
+
+export default MainContent;
---
+++ src/Navigation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Navigation() {
+    return (
+        <nav>
+            <ul>
+                <li>test</li>
+                <li>test</li>
+                <li>test</li>
+                <li>test</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navigation;
